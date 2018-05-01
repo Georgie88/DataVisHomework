33a809877ebe680db288949540afb8798de1acb2
Giorgia <giorgia.basso.88@gmail.com>
2018-05-01 18:01:58 -0500

    homework web scrape no sql

---
+++ 13. Web scrape nosql/app.py
@@ -0,0 +1,40 @@
+# import dependencies
+import time
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import pymongo
+import scrape_mars
+
+app = Flask(__name__)
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_info
+collection = db.mars_scrape
+
+# basic app route
+@app.route("/")
+def index():
+    mars_info = db.collection.find_one()
+    return render_template("index.html", mars_info=mars_info)
+
+# app route for the webscraper
+@app.route("/scrape")
+def scrape():
+    # create the mongodb and fill it with the webscraper results
+    mars_info = db.collection
+    mars_data = scrape_mars.scrape()
+    #print(mars_data)
+    mars_info.update(
+        {},
+        mars_data,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
